98f5f0338d6619dbf442ee0433d6617838eb85f2
Keith Dahlby <keith@deltavcodeschool.com>
2021-10-08 15:55:06 -0500

    Add Node.Next

--- c-sharp/DataStructures/DataStructuresTests/LinkedListTests.cs
+++ c-sharp/DataStructures/DataStructuresTests/LinkedListTests.cs
@@ -30,8 +30,14 @@ namespace DataStructuresTests
       list.Insert(1);
 
       // Assert
+      // Make sure we have a Head node
       Assert.NotNull(list.Head);
+
+      // Make sure the Head node has the inserted Value
       Assert.Equal(1, list.Head.Value);
+
+      // Make sure the Head node does not have a Next
+      Assert.Null(list.Head.Next);
     }
   }
 }
