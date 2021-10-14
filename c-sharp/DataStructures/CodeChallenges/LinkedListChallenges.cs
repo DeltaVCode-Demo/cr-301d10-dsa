99d0be2dc1d9b5030b72823832df850cb3edb26f
Keith Dahlby <keith@deltavcodeschool.com>
2021-10-14 15:15:28 -0500

    ZipTwoLists()

--- c-sharp/DataStructures/CodeChallenges/LinkedListChallenges.cs
+++ c-sharp/DataStructures/CodeChallenges/LinkedListChallenges.cs
@@ -1,4 +1,3 @@
-using System;
 using DataStructures;
 
 namespace CodeChallenges
@@ -10,8 +9,29 @@ namespace CodeChallenges
       if (list1.Head == null)
         return list2;
 
-      //// TODO: Draw the rest of the owl
+      Node c1 = list1.Head;
+      Node c2 = list2.Head;
 
+      while (c1 != null && c2 != null)
+      {
+        // list1: (A) -> (C) -> (E) ->
+        // list2: (B) -> (D) -> (F) ->
+
+        Node n1 = c1.Next;
+        c1.Next = c2; // point at (B)
+        c1 = n1;      // Move c1 to (C)
+
+        Node n2 = c2.Next;
+
+        // Don't lose the rest of l2 if c1 is done
+        if (c1 != null)
+        {
+          c2.Next = c1; // point at (C)
+        }
+        c2 = n2;      // Move c2 to (D)
+      }
+
+      // We think we're done!
       return list1;
     }
   }
--- c-sharp/DataStructures/CodeChallengesTests/LinkedListCodeChallengeTests.cs
+++ c-sharp/DataStructures/CodeChallengesTests/LinkedListCodeChallengeTests.cs
@@ -53,5 +53,73 @@ namespace CodeChallengesTests
       // Assert
       Assert.Equal(new[] { 1 }, result);
     }
+
+    [Fact]
+    public void ZipTwoLists_with_singleton_lists()
+    {
+      // Arrange
+      LinkedList list1 = new LinkedList();
+      list1.Insert(1);
+
+      LinkedList list2 = new LinkedList();
+      list2.Insert(2);
+
+      // Act
+      LinkedList result = LinkedListChallenges.ZipTwoLists(list1, list2);
+
+      // Assert
+      Assert.Equal(new[] { 1, 2 }, result);
+    }
+
+    [Fact]
+    public void ZipTwoLists_with_bigger_equal_length_lists()
+    {
+      // Arrange
+      LinkedList list1 = new LinkedList();
+      list1.Insert(1, 3, 5);
+
+      LinkedList list2 = new LinkedList();
+      list2.Insert(2, 4, 6);
+
+      // Act
+      LinkedList result = LinkedListChallenges.ZipTwoLists(list1, list2);
+
+      // Assert
+      Assert.Equal(new[] { 1, 2, 3, 4, 5, 6 }, result);
+    }
+
+    [Fact]
+    public void ZipTwoLists_with_larger_list1()
+    {
+      // Arrange
+      LinkedList list1 = new LinkedList();
+      list1.Insert(1, 3, 5, 7);
+
+      LinkedList list2 = new LinkedList();
+      list2.Insert(2, 4);
+
+      // Act
+      LinkedList result = LinkedListChallenges.ZipTwoLists(list1, list2);
+
+      // Assert
+      Assert.Equal(new[] { 1, 2, 3, 4, 5, 7 }, result);
+    }
+
+    [Fact]
+    public void ZipTwoLists_with_larger_list2()
+    {
+      // Arrange
+      LinkedList list1 = new LinkedList();
+      list1.Insert(1, 3);
+
+      LinkedList list2 = new LinkedList();
+      list2.Insert(2, 4, 6, 8);
+
+      // Act
+      LinkedList result = LinkedListChallenges.ZipTwoLists(list1, list2);
+
+      // Assert
+      Assert.Equal(new[] { 1, 2, 3, 4, 6, 8 }, result);
+    }
   }
 }
