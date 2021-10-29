5c6b9dcbe68b0a9f278e6f9047a2418d89e97e6d
Keith Dahlby <keith@deltavcodeschool.com>
2021-10-29 09:48:16 -0500

    Add SumOdds()

--- c-sharp/DataStructures/CodeChallenges/BinaryTreeChallenges.cs
+++ c-sharp/DataStructures/CodeChallenges/BinaryTreeChallenges.cs
@@ -18,5 +18,29 @@ namespace CodeChallenges
         + Sum(node.Left)
         + Sum(node.Right);
     }
+
+    public static int SumOdds(BinaryTree<int> tree)
+    {
+      return SumOdds(tree.Root);
+
+      // Local Function - even more private than private
+      int SumOdds(Node<int> node)
+      {
+        if (node == null) return 0;
+
+        int sum = 0;
+
+        // Only add odd value
+        if (node.Value % 2 != 0)
+        {
+          sum += node.Value;
+        }
+
+        sum += SumOdds(node.Left);
+        sum += SumOdds(node.Right);
+
+        return sum;
+      }
+    }
   }
 }
--- c-sharp/DataStructures/CodeChallengesTests/BinaryTreeChallengeTests.cs
+++ c-sharp/DataStructures/CodeChallengesTests/BinaryTreeChallengeTests.cs
@@ -88,5 +88,26 @@ namespace CodeChallengesTests
       // Assert
       Assert.Equal(26, result);
     }
+
+    [Fact]
+    public void SumOdd_returns_sum_of_odds_in_tree_with_grandchildren()
+    {
+      // Arrange
+      BinaryTree<int> tree = new BinaryTree<int>();
+      tree.Root = new Node<int> { Value = 7 };
+      tree.Root.Left = new Node<int> { Value = 3 };
+      tree.Root.Left.Left = new Node<int> { Value = 1 };
+      tree.Root.Left.Right = new Node<int> { Value = 2 };
+      tree.Root.Right = new Node<int> { Value = 4 };
+      tree.Root.Right.Left = new Node<int> { Value = 6 };
+      tree.Root.Right.Right = new Node<int> { Value = 8 };
+      tree.Root.Right.Right.Right = new Node<int> { Value = -5 };
+
+      // Act
+      int result = BinaryTreeChallenges.SumOdds(tree);
+
+      // Assert
+      Assert.Equal(6, result);
+    }
   }
 }
