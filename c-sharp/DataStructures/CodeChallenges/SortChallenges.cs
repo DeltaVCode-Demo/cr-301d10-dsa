31a0bff3b610648279a53a5b1566053d2d7e35ce
Keith Dahlby <keith@deltavcodeschool.com>
2021-11-09 20:14:35 +0000

    Generic sort with IComparer<T> support

--- c-sharp/DataStructures/CodeChallenges/SortChallenges.cs
+++ c-sharp/DataStructures/CodeChallenges/SortChallenges.cs
@@ -1,27 +1,33 @@
+using System;
+using System.Collections.Generic;
+
 namespace CodeChallenges
 {
   public static class SortChallenges
   {
-    public static void MergeSort(int[] input)
+    public static void MergeSort<T>(T[] input) where T : IComparable<T>
+      => MergeSort(input, Comparer<T>.Default);
+
+    public static void MergeSort<T>(T[] input, IComparer<T> comparer) where T : IComparable<T>
     {
       if (input.Length <= 1) return;
 
       int mid = input.Length / 2;
-      int[] left = input[..mid];
-      int[] right = input[mid..];
+      T[] left = input[..mid];
+      T[] right = input[mid..];
 
-      MergeSort(left);
-      MergeSort(right);
+      MergeSort(left, comparer);
+      MergeSort(right, comparer);
 
-      Merge(left, right, input);
+      Merge(left, right, input, comparer);
     }
 
-    private static void Merge(int[] left, int[] right, int[] input)
+    private static void Merge<T>(T[] left, T[] right, T[] input, IComparer<T> comparer) where T : IComparable<T>
     {
       int i = 0, l = 0, r = 0;
 
       while (l < left.Length && r < right.Length)
-        input[i++] = left[l] < right[r] ? left[l++] : right[r++];
+        input[i++] = comparer.Compare(left[l], right[r]) <= 0 ? left[l++] : right[r++];
 
       while (l < left.Length)
         input[i++] = left[l++];
